b014b5d7061f55ee3ba134f657461759394e789e
Maeght Loan <37497007+loan-mgt@users.noreply.github.com>
2025-05-18 16:24:05 +0200

    Update content_analyser.py

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -74,9 +74,7 @@ def analyse_stream(vision_frame : VisionFrame, video_fps : Fps) -> bool:
 
 
 def analyse_frame(vision_frame : VisionFrame) -> bool:
-	nsfw_scores = detect_nsfw(vision_frame)
-
-	return len(nsfw_scores) > 0
+	return False
 
 
 @lru_cache(maxsize = None)
@@ -87,26 +85,7 @@ def analyse_image(image_path : str) -> bool:
 
 @lru_cache(maxsize = None)
 def analyse_video(video_path : str, trim_frame_start : int, trim_frame_end : int) -> bool:
-	video_fps = detect_video_fps(video_path)
-	frame_range = range(trim_frame_start, trim_frame_end)
-	rate = 0.0
-	total = 0
-	counter = 0
-
-	with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =', disable = state_manager.get_item('log_level') in [ 'warn', 'error' ]) as progress:
-
-		for frame_number in frame_range:
-			if frame_number % int(video_fps) == 0:
-				vision_frame = read_video_frame(video_path, frame_number)
-				total += 1
-				if analyse_frame(vision_frame):
-					counter += 1
-			if counter > 0 and total > 0:
-				rate = counter / total * 100
-			progress.set_postfix(rate = rate)
-			progress.update()
-
-	return rate > 10.0
+	return False
 
 
 def detect_nsfw(vision_frame : VisionFrame) -> List[Score]:
